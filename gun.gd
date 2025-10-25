8386afcb1afaaa9525afe7692934692b621deac5
spasterz <steven.pasterz@gmail.com>
2025-10-25 00:33:39 -0700

    added pistol

--- gun.gd
+++ gun.gd
@@ -1,4 +1,7 @@
 extends Area2D
 
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target_enemy = enemies_in_range[0]
+		look_at(target_enemy.global_position)
